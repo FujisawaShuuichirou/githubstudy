ca379b0d6bd7f5a1893016e7144d562253ae73f2
Fujisawa_Shuuichirou <s21c1104dw@s.chibakoudai.jp>
2021-06-04 23:06:52 +0900

    files ouload

--- test2.c
+++ test2.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int i = 1;
+int i,x = 1;
 
 int main()
 {
-    while(1);
+    while(1)
     {
-        for(;i <= 10;i++)
+        for(i = 1;i <= x;i++)
         {
+            x++;
             printf("%d回目の繰り返しです。\n",i);
+            //wait(1);
         }
-
-        if (i >= 100)
-            break;
+        //break;
     }
 
 }
